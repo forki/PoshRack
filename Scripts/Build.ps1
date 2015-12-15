3b825d5b966658c2da97377adaadb14116978da5
Don Schenck <don.schenck@gmail.com>
2015-12-15 22:12:20 +0000

    Fixes #29

--- Scripts/Build.ps1
+++ Scripts/Build.ps1
@@ -2,18 +2,27 @@
 # Build.ps1
 #
 
+# If NUGET is NOT installed on the build machine, install it here and now.
+if(!( test-path $env:LocalAppData\NuGet\NuGet.exe)) {
+  # download nuget
+  Invoke-WebRequest 'https://www.nuget.org/nuget.exe' -OutFile "$($env:LocalAppData)\NuGet\NuGet.exe"
+}
+
+
+# If Pester is NOT installed on the build machine, install it here and now.
+if(!(Test-Path $env:HOMEPATH\Documents\WindowsPowerShell\Modules\Pester\tools\pester.psm1)) {
+	New-Item -ItemType Directory -Force "$($env:HOMEPATH)\Documents\WindowsPowerShell\Modules"
+	$CMD = "$($env:LocalAppData)\NuGet\NuGet.exe"
+	Start-Process -FilePath $CMD -ArgumentList "Install  Pester -ExcludeVersion -o $($env:HOMEPATH)\Documents\WindowsPowerShell\Modules" -Wait
+	Set-Location "$env:HOMEPATH\Documents\WindowsPowerShell\Modules\Pester"
+	Invoke-Pester
+}
 
 # Run Pester tests and output to NUnit-compatible XML file. This
 # allows the CICD (Bamboo.com) to pick up the results.
 Invoke-Pester -OutputXml
 
 
-
-# If NUGET is NOT installed on the build machine, install it here and now.
-if(!( test-path $env:LocalAppData\NuGet\NuGet.exe)) {
-  # download nuget
-  Invoke-WebRequest 'https://www.nuget.org/nuget.exe' -OutFile "$($env:LocalAppData)\NuGet\NuGet.exe"
-}
 
 # Run PACK to build the Nuget package
 $CMD = "$env:LocalAppData\NuGet\NuGet.exe"
