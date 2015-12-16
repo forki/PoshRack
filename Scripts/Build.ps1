e835b1f862a0d73fac97f3855570216127f366ef
Don Schenck <don.schenck@gmail.com>
2015-12-16 14:41:06 +0000

    Fixes #32

--- Scripts/Build.ps1
+++ Scripts/Build.ps1
@@ -10,16 +10,14 @@ if(!( test-path $env:LocalAppData\NuGet\NuGet.exe)) {
 
 
 # If Pester is NOT installed on the build machine, install it here and now.
-if(!(Test-Path $env:HOMEPATH\Documents\WindowsPowerShell\Modules\Pester\tools\pester.psm1)) {
-	New-Item -ItemType Directory -Force "$($env:HOMEPATH)\Documents\WindowsPowerShell\Modules"
-	$CMD = "$($env:LocalAppData)\NuGet\NuGet.exe"
-	Start-Process -FilePath $CMD -ArgumentList "Install  Pester -ExcludeVersion -o $($env:HOMEPATH)\Documents\WindowsPowerShell\Modules" -Wait
-	Set-Location "$env:HOMEPATH\Documents\WindowsPowerShell\Modules\Pester"
-	Invoke-Pester
+if(-not(Get-Module -Name "Pester")) {
+	(new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex
+	Install-Module Pester -Global 
 }
 
 # Run Pester tests and output to NUnit-compatible XML file. This
 # allows the CICD (Bamboo.com) to pick up the results.
+Import-Module Pester
 Invoke-Pester -OutputXml
 
 
